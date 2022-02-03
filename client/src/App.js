56d27eb1959098715b68adbb7b9750302f068ae0
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2022-02-03 01:45:59 -0500

    added animation on scroll

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AOS from "aos";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
-
 import "./styles/App.css";
+import "aos/dist/aos.css";
 
 function App() {
+  // AOS init
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div className="app-wrapper">
       <Router>
